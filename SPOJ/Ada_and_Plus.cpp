b7d68215c78de95cca20bfb97b260242595528d3
Piotr Czapla <234751@edu.p.lodz.pl>
2023-04-14 00:24:20 +0100

    qq

--- SPOJ/Ada_and_Plus.cpp
+++ SPOJ/Ada_and_Plus.cpp
@@ -8,18 +8,16 @@
 #include <unordered_map>
 #include <cmath>
 using namespace std;
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+void solution1(){
     int t;
     cin>>t;
     for(int i =0;i<t;i++){
-       // vector<vector<char>> vec(n)(n);
-         int n;
+        // vector<vector<char>> vec(n)(n);
+        int n;
         cin>>n;
 //        char** arr = new char*[n];
 //        for(int z = 0; z < n; ++z) arr[z] = new char[n];
-       // char arr[n][n];
+        // char arr[n][n];
 //        int *histogramx = new int(n);
 //        int *histogramy = new int(n);
         vector<int> histogramx;
@@ -42,7 +40,7 @@ int main() {
                     histogramy[c]++;
                 }
             }
-           // cout<<'\n';
+            // cout<<'\n';
         }
 //        for(int p = 0 ; p < n ; p++){
 //            cout<<histogramx[p]<<" - ";
@@ -70,8 +68,103 @@ int main() {
 //        delete  histogramx;
 //        delete  histogramy;
 //        delete [] arr;
-    cout<<'\n';
+        cout<<'\n';
     }
+}
+void solution2(){
+    int t;
+    cin>>t;
+    for(int i =0;i<t;i++){
+        // vector<vector<char>> vec(n)(n);
+        int n;
+        cin>>n;
+//        char** arr = new char*[n];
+//        for(int z = 0; z < n; ++z) arr[z] = new char[n];
+        // char arr[n][n];
+//        int *histogramx = new int(n);
+//        int *histogramy = new int(n);
+        vector<int> histogramx;
+        vector<int> histogramy;
+//        //char histogramx[n]={0};
+//        //char histogramy[n]={0};
+        for(int p = 0 ; p < n ; p++){
+//            histogramx[p]=0;
+//            histogramy[p]=0;
+            histogramx.push_back(0);
+            histogramy.push_back(0);
+        }
+        for(int p = 0 ; p < n ; p++){
+            for(int c = 0 ; c < n ; c++){
+                char a;
+                cin>>a;
+//                arr[p][c]=a;
+                if(a=='#'){
+                    histogramx[p]++;
+                    histogramy[c]++;
+                }
+            }
+            // cout<<'\n';
+        }
+//        for(int p = 0 ; p < n ; p++){
+//            cout<<histogramx[p]<<" - ";
+//        }
+//        cout<<'\n';
+//        for(int p = 0 ; p < n ; p++){
+//            cout<<histogramy[p]<<" - ";
+//        }
+        int result=1;
+        pair<int,int> max_pair;
+        max_pair.first=0;
+        max_pair.second=0;
+       // std::sort(histogramx.begin(), histogramx.end());
+       // std::sort(histogramy.begin(), histogramy.end());
+        // for(int p = n-1 ; p >= 0 ; p--){
+        for(int p =0  ; p < n-1; p++){
+            //cout<<histogramx[p]<<" - "<<histogramy[p]<<" , ";
+            //cout<<max_pair.first<<" - "<<max_pair.second<<" , ";
+
+            if((histogramx[p]>max_pair.first) && (histogramy[p]>max_pair.second)){
+                //result=histogramx[p];
+                //break;
+                max_pair.first=histogramx[p];
+                max_pair.second=histogramy[p];
+            }
+            //cout<<'\n';
+        }
+        result=min(max_pair.first,max_pair.second);
+        if(result%2==0){
+            cout<<result/2;
+        }else{
+            cout<<result/2+1;
+        }
+//        if(max_pair.first%2==0 && max_pair.second%2==0){
+//            cout<<result/2;
+//        }else{
+//            cout<<result/2+1;
+//        }
+//        delete  histogramx;
+//        delete  histogramy;
+//        delete [] arr;
+        cout<<'\n';
+    }
+}
+void solution3(){
+    int t;
+    cin>>t;
+    for(int i =0;i<t;i++){
+        int n;
+        cin>>n;
+        for(int p = 0 ; p < n ; p++){
+
+        }
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //solution1();
+    solution2();
+    //solution3();
 
     return 0;
 }
--- SZKOPUL/flappy.cpp
+++ SZKOPUL/flappy.cpp
@@ -37,9 +37,7 @@ class object{
         top=par;
     }
 };
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+void solution1(){
     int n,x;
     cin>>n>>x;
     //unordered_map<int, pair<int,int>> umap;
@@ -57,12 +55,12 @@ int main() {
 //        cin>>a>>bottom>>top;
 //        umap[a].first=bottom;
 //        umap[a].second=top;
-          int a,x,y;
-          cin>>a>>x>>y;
-          object o(a,x,y);
-          objects.push_back(o);
-          vec[a].set_bottom(x);
-          vec[a].set_top(y);
+        int a,x,y;
+        cin>>a>>x>>y;
+        object o(a,x,y);
+        objects.push_back(o);
+        vec[a].set_bottom(x);
+        vec[a].set_top(y);
     }
     int counter=0;
     int iter=0;
@@ -70,7 +68,7 @@ int main() {
         if(bird.first==objects[iter].get_a()){
             iter++;
         }
-       // cout<<"object number - "<<iter+1<<" field number - "<<i<<" x ptaka - "<<bird.first<<" y ptaka - "<<bird.second<<'\n';
+        // cout<<"object number - "<<iter+1<<" field number - "<<i<<" x ptaka - "<<bird.first<<" y ptaka - "<<bird.second<<'\n';
         if(
                 ((objects[iter].get_bottom()+1) - bird.second) == objects[iter].get_a() - bird.first
                 &&
@@ -91,7 +89,9 @@ int main() {
             bird.second++;
             counter++;
             //cout<<"JUMP! 2"<<'\n';
-        }else{bird.second--;}
+        }else{
+            bird.second--;
+        }
         bird.first++;
     }
 
@@ -109,11 +109,81 @@ int main() {
 //            //cout<<"counter 2 - "<<sqrt(pow(pow(i.get_a()-bird.first,2)+pow((i.get_bottom()+1)-bird.first,2),2))<<'\n';
 //        }
 //    }
-    if(counter>=0){
+    if(counter>0){
         cout<<counter;
     }else{
         cout<<"NIE";
     }
+}
+void solution2(){
+    /*
+     * n -  liczba przeszkód
+     * x - położenie mety
+     */
+    int n,x;
+    cin>>n>>x;
+    //unordered_map<int, pair<int,int>> umap;
+    int bird_y=0;
+    vector<pair<int,pair<int,int>>> vec;
+    for(int i = 0 ;i<n;i++){
+        int x,a,b;
+        cin>>x>>a>>b;
+        pair<int,int> a_b;
+        pair<int,pair<int,int>> res;
+        a_b.first=a;
+        a_b.second=b;
+        res.first=x;
+        res.second=a_b;
+        vec.push_back(res);
+    }
+//    for(auto&& i : vec){
+//        cout<<" x = "<<i.first<<" a = "<<i.second.first<<" b = "<<i.second.second<<'\n';
+//    }
+    int counter=0;
+    int jumps=0;
+    bool flag=false;
+    for(int i = 0 ;i<=x;i++){
+            //cout<<"counter["<<i<<"] "<<counter[i]<<" bird_y = "<<bird_y<<" jumps = "<<jumps<<'\n';
+            //cout<<" bird_y = "<<bird_y<<" i = "<<i<<" counter = "<<counter<<" vec[counter].first = "<<vec[counter].first<<'\n';
+            //cout<<" bird_y = "<<bird_y<<" vec[counter].second.second = "<<vec[counter].second.second<<" vec[counter].second.first = "<<vec[counter].second.first<<" vec[counter].first = "<<vec[counter].first<<" i = "<<i<<'\n';
+           // cout<<" bcount = "<<bcount<<'\n';
+            if((bird_y>vec[counter].second.second && vec[counter].first==i) || (bird_y<vec[counter].second.first && vec[counter].first==i)){
+                flag= true;
+                break;
+            }
+            if((bird_y>vec[counter].second.second && vec[counter].first==i) || (bird_y<vec[counter].second.first && vec[counter].first==i)){
+                flag= true;
+                break;
+            }
+            if(vec[counter].first==i){
+                counter++;
+            }
+            if(vec[counter].second.first>=bird_y && ((vec[counter].second.first+1)-bird_y == vec[counter].first - i)){
+                bird_y++;
+                jumps++;
+            }else if(vec[counter].second.first<bird_y && (bird_y-vec[counter].second.first ==  i-vec[counter].first)){
+                bird_y++;
+                jumps++;
+            }
+//            else if(vec[counter].second.first>=bird_y && ((vec[counter].second.first+1)-bird_y > vec[counter].first - i)){
+//                flag= true;
+//                break;
+//            }
+            else{
+                bird_y--;
+            }
+    }
+    if(flag){
+        cout<<"NIE";
+    }else{
+        cout<<jumps;
+    }
 
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //solution1();
+    solution2();
     return 0;
 }
