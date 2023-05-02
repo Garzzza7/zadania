3d341d13a58da59d67be86f21727c819aa120577
Garzzza7 <piotr.czapela@gmail.com>
2023-05-02 01:34:09 +0100

    sdf

--- n.cpp
+++ n.cpp
@@ -8,24 +8,36 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin>>t;
-    for(int i = 0 ; i < t ; i++){
-        int l,r;
-        cin>>l>>r;
-        vector<int> numbers;
-        int counter=0;
-        for(int i = l ; i<=r;i++){numbers.push_back(i);}
-        for(int i = 0;i<numbers.size();i++){
-            int counter1=0;
-            for(int j = 0;j<numbers.size();j++){
-                if(std::count(numbers.begin(), numbers.end(),i*2)){
-                    counter1++;
-                }
-            }
-            if(counter1>counter){counter=counter1;}
-        }
-    cout<<counter<<'\n';
+    int n,k;
+    cin>>n>>k;
+    vector<int> lec;
+    vector<int> wake;
+
+    for(int i = 0 ; i < n ; i++){
+        int a;
+        cin>>a;
+        lec.push_back(a);
     }
+    for(int i = 0 ; i < n ; i++){
+        int a;
+        cin>>a;
+        wake.push_back(a);
+    }
+    int max = 0 ;
+    int counter = 0 ;
+    int index=0;
+    for(int i = 0 ; i < n ; i++){
+        if(wake[i]==0){
+            counter++;
+        }if(wake[i]==1){
+            if(counter>max){
+                max = counter;
+                index = i - counter;
+            }
+            counter =0;
+        }
+        cout<<index<<" - "<<max<<'\n';
+    }
+
     return 0;
 }
