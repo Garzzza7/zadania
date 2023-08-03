1e1e049364eec5e2f46f662c497ca64f1fa32135
Garzzza7 <piotr.czapela@gmail.com>
2023-08-03 19:47:18 +0200

    bruh

--- CodeForces/Fibonaccharsis.cpp
+++ CodeForces/Fibonaccharsis.cpp
@@ -1,15 +1,52 @@
 #include <bits/stdc++.h>
-
+#include <cmath>
 using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     int t;
+    cin>>t;
     while(t--){
-        int n,k;
+        long long n,k;
         cin>>n>>k;
-        vector<int> vec(k,0);
-        vec[k-1]=n;
+        int counter=0;
+        if(n==1 & k==3){
+            counter=1;
+        }
+        else if(k>n+2){
+            counter=0;
+        }else{
+            for(int z = 0 ; z <= n/2 ; z++){
+                vector<long long> vec(k);
+                vec[k-1]=n;
+                if(n%2==0){
+                    vec[k-2]=n/2+z;
+                    vec[k-3]=n/2-z;
+                }else{
+                    vec[k-2]=n/2+z+1;
+                    vec[k-3]=n/2-z;
+                }
+                for(long long i  = k-3 ; i >= 0 ; i--){
+                    if(vec[i+1]<(vec[i+2]-vec[i+1])){
+
+                        break;
+
+                    }
+                    vec[i]=vec[i+2]-vec[i+1];
+                    if(i==0){
+                        counter++;
+//                        for(auto&& a : vec){
+//                            cout<<a<<" - ";
+//                        }
+//                        cout<<"\n";
+                    }
+                }
+
+            }
+       }
+
+        cout<<counter<<"\n";
+        //printf("%d\n",counter);
     }
     return 0;
 }
--- CodeForces/Same_Differences.cpp
+++ CodeForces/Same_Differences.cpp
@@ -1,40 +1,52 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+long long sumup(long long a){
+    long long res=0;
+    for(long long i = 0 ; i < a ; i++){
+        res+=i;
+    }
+    return res;
+}
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int t;
+    long long t;
     cin>>t;
     while(t--){
-        vector<int> vec;
-        int n;
+        vector<long long> vec;
+        vector<long long> sums;
+        long long n;
         cin>>n;
-        for(int i = 0 ; i < n ; i++){
+        for(long long i = 0 ; i < n ; i++){
             int a;
             cin>>a;
             vec.push_back(a);
         }
-        int counter=0;
-        for(int j = 1 ; j < n ; j++){
-            for(int i = 0 ; i < j ; i++){
-                if(vec[j]-vec[i]==j-i){
-                    counter++;
-                }
-                //printf("vec[%d] = %d and vec[%d] = %d\n",j,vec[j],i,vec[i]);
+        for(long long i = 0 ; i < vec.size() ; i++){
+            sums.push_back(vec[i]-i);
+        }
+        std::sort(sums.begin(), sums.end());
+        //std::reverse(sums.begin(), sums.end());
+//        for(auto&& a : sums){
+//            cout<<a<<" - ";
+//        }
+       // cout<<sums[0]<<" - "<<sums[sums.size()-1];
+        long long sum=0;
+        long long counter=0;
+        sums.push_back(0);
+        for(long long i = 0 ; i < sums.size() ;i++){
+            counter++;
+            if(sums[i]!=sums[i+1]){
+                sum+= sumup(counter);
+                counter=0;
             }
         }
-        printf("%d\n",counter);
+        //cout<<"\n";
+        cout<<sum<<"\n";
     }
 
 
     return 0;
 }
-
-
-
-
-
-
-
 
