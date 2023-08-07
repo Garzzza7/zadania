f469db5d9519c77fb247f7a71fa89c64336004a8
Garzzza7 <piotr.czapela@gmail.com>
2023-08-07 20:30:41 +0200

    asdf

---
+++ CodeForces/Football.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+void print_result(int a,int b){
+    
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,a,b;
+    cin>>n;
+    cin>>a;
+    cin>>b;
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
