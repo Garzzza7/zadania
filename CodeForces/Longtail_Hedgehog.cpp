7a92106c28411ca1c3d9f21dfecf793874e4f5df
Garzzza7 <piotr.czapela@gmail.com>
2023-10-01 19:48:01 +0200

    e

--- CodeForces/Longtail_Hedgehog.cpp
+++ CodeForces/Longtail_Hedgehog.cpp
@@ -2,11 +2,11 @@
 #include <cmath>
 #include <stdlib.h>
 using namespace std;
-//https://codeforces.com/problemset/problem/1418/C
+//https://codeforces.com/problemset/problem/615/B
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-     
+    
     return 0;
 }
 
