5a6307ec29114f6b646fa0b14a5dcb920fb1324b
Garzzza7 <piotr.czapela@gmail.com>
2023-10-04 16:20:31 +0200

    sda

---
+++ CodeForces/Xenia_and_Weights.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <stdlib.h>
+//https://codeforces.com/problemset/problem/339/C
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
