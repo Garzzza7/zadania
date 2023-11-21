1f8e41efab26a5ede9d510c59a378e1530c2fe1d
Garzzza7 <piotr.czapela@gmail.com>
2023-11-21 16:17:51 +0100

    as

--- CodeForces/Prefix_Purchase.cpp
+++ CodeForces/Prefix_Purchase.cpp
@@ -2,70 +2,78 @@
 #include <cmath>
 #include <stdlib.h>
 using namespace std;
-vector<int> a(2000001);
-
-// https://codeforces.com/problemset/problem/1870/D
-void lex(vector<int> &vec, int &price, vector<int> &a,int n)
-{
-    int smallest = INT32_MAX;
-    int index;
-    for (int i = 0; i < vec.size(); i++)
-    {
-        if (vec[i] <= smallest /*&& vec[i] <= price*/)
-        {
-            smallest = vec[i];
-            index = i;
-        }
-    }
-    cout<<"SMALLEST = "<<smallest<<" at index "<<index<<"\n";
-    if ((price - smallest < 0) || (index==vec.size()-1) || price==0)
-    {
-        for(int i = 0 ; i < n ; i++){
-            cout<<a[i]<<" ";
-        }cout<<"\n";
-    }
-    else
-    {
-        while (true)
-        {
-            if (price - smallest <= 0)
-            {
-                break;
-            }
-            price-=smallest;
-            for (int i = 0; i <= index; i++)
-            {
-                a[i]++;
-            }
-        }
-        vector<int> buffer;
-        for (int i = index+1; i < vec.size(); i++)
-        {
-            buffer.push_back(vec[i]);
-        }
-        lex(buffer,price,a,n);
-    }
-}
-int main()
-{
+const long long mod = 1000000007;
+class Node{
+    public:
+        int index=0;
+        int cost=0;
+        double ratio=0;
+};
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     int t;
-    cin >> t;
+    cin>>t;
     while(t--){
-    int n;
-    cin >> n;
-    vector<int> c;
-    for (int i = 0; i < n; i++)
-    {
-        int in;
-        cin >> in;
-        c.push_back(in);
-    }
-    int k;
-    cin >> k;
-    lex(c,k,a,n);
-    }
+        int n;
+        cin>>n;
+        vector<Node> vec;
+        vector<int> a(n,0);
+        for(int i =1 ; i<= n ; i++){
+            int a;
+            cin>>a;
+            Node node;
+            node.index=i;
+            node.cost=a;
+            node.ratio=(double)((double)a/(double)i);
+            vec.push_back(node);
+        }
+        int k;
+        cin>>k;
+        std::sort(vec.begin(), vec.end(),[](Node const & a, Node const & b) -> bool { return a.ratio < b.ratio; } );
+        // for(auto&& a : vec){
+        //     cout<<a.ratio<<" - ";
+        // }
+        // cout<<'\n';
+        int iter=0;
+        while(k>0){
+            if(k<vec[iter].cost){
+                iter++;
+                // if(iter>n){
+                //     iter=n;
+                // }
+            }
 
+            // else
+            // {
+                if(k-vec[iter].cost>vec[iter].cost){
+                    iter++;
+                }
+                cout << " iter = " << iter << " k = " << k << " cost " << vec[iter].cost << " index = "<<vec[iter].index<< "\n";
+                for (int i = 0; i < vec[iter].index; i++)
+                {
+                    a[i]++;
+                }
+                // if (k - vec[iter].cost < 0)
+                // {
+                    // break;
+                // }
+                k -= vec[iter].cost;
+            // }
+            // cout << " k = " << k;
+        }
+        for(auto&& b : a){
+            cout<<b<<" ";
+        }
+        cout<<"\n";
+    }
     return 0;
 }
+
+
+
+
+
+
+
+
