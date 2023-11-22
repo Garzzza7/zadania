647697b54e6ad20d9c4885ffcdb4de8f30e6b01e
Garzzza7 <piotr.czapela@gmail.com>
2023-11-23 00:18:10 +0100

    asd

--- AtCoder/Disjoint_Set_Union.cpp
+++ AtCoder/Disjoint_Set_Union.cpp
@@ -2,32 +2,63 @@
 #include <cmath>
 #include <stdlib.h>
 using namespace std;
-//https://atcoder.jp/contests/practice2/tasks/practice2_a
-const long long mod = 1000000007;
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n,q;
-    cin>>n>>q;
-    vector<vector<int>> vec(n+1,vector<int>());
-    for(int i = 0 ; i < q ; i++){
-        int a,b,c;
-        cin>>a>>b>>c;
-        if(a==0){
-            vec[b].push_back(c);
-        }else{
-            if(true){
+// https://atcoder.jp/contests/practice2/tasks/practice2_a
 
-            }
-        }
-    } 
-    return 0;
+const long long mod = 1000000007;
+int vec[2000001];
+int find_set(int v)
+{
+    if (vec[v] == v)
+    {
+        return v;
+    }
+    // else
+    // {
+        // return vec[v] = find(vec[v]);
+    // }
+    return find_set(vec[v]);
 }
 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, q;
+    cin >> n >> q;
+    for (int i = 0; i < n; i++)
+    {
+        vec[i] = i;
+    }
+    // vector<vector<int>> vec(n+1,vector<int>());
+    for (int i = 0; i < q; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a == 0)
+        {
+            vec[c] = b;
+            // vec[b] = c;
+        }
+        else
+        {
+            int res1 = find_set(b);
+            int res2 = find_set(c);
+            // cout<<"RES1 = "<<res1<<" RES2 = "<<res2<<'\n';
+            if (res1 == res2)
+            {
+                cout << '1';
+            }
+            else
+            {
+                cout << '0';
+            }
 
-
-
-
-
-
-
+            cout << '\n';
+        }
+    }
+    // for (int i = 0; i < n ; i++)
+    // {
+    //     cout<<i<<" - "<<vec[i]<<'\n';
+    // }
+    return 0;
+}
---
+++ AtCoder/Fenwick_Tree.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+const long long mod = 1000000007;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,q;
+    cin>>n>>q;
+    vector<int> prefix(n+1,0);
+    for(int i = 1 ; i<=n  ;i++){
+        int a;
+        cin>>a;
+        prefix[i]+=(prefix[i-1]+a);
+    } 
+    return 0;
+}
+
+
+
+
+
+
+
+
