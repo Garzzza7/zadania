d8a9ac9d65afa8eb3f3160926e3440fdbc9e198f
Garzzza7 <piotr.czapela@gmail.com>
2023-12-03 22:02:56 +0100

    KMP

--- Algos/Disjoint_Set_Union.cpp
+++ Algos/Disjoint_Set_Union.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 const long long mod = 1000000007;
 // int vec[2000001];
-int find_set(long long v,vector<long long> vec)
+int find_set(long long v, vector<long long> vec)
 {
     if (vec[v] == v)
     {
         return v;
     }
-    return vec[v]=find_set(vec[v],vec);
+    return vec[v] = find_set(vec[v], vec);
     // return vec[v];
 }
 int main()
@@ -27,8 +27,8 @@ int main()
     for (long long i = 0; i < n; i++)
     {
         vec[i] = i;
-        size[i]=1;
-        rank[i]=0;
+        size[i] = 1;
+        rank[i] = 0;
     }
     // vector<vector<int>> vec(n+1,vector<int>());
     for (long long i = 0; i < q; i++)
@@ -43,22 +43,25 @@ int main()
             //     vec[c] = b;
             // }
 
-            long long bb = find_set(b,vec);
-            long long cc = find_set(c,vec);
-            if(bb!=cc){
-                if(rank[bb]<rank[cc]){
-                    swap(bb,cc);
+            long long bb = find_set(b, vec);
+            long long cc = find_set(c, vec);
+            if (bb != cc)
+            {
+                if (rank[bb] < rank[cc])
+                {
+                    swap(bb, cc);
                 }
-                vec[cc]=bb;
-                if(rank[bb]==rank[cc]){
+                vec[cc] = bb;
+                if (rank[bb] == rank[cc])
+                {
                     rank[bb]++;
                 }
             }
         }
         else
         {
-            long long res1 = find_set(b,vec);
-            long long res2 = find_set(c,vec);
+            long long res1 = find_set(b, vec);
+            long long res2 = find_set(c, vec);
             // cout<<"RES1 = "<<res1<<" RES2 = "<<res2<<'\n';
             if (res1 == res2)
             {
---
+++ Algos/KMP.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+const long long mod = 1000000007;
+bool hasSubstring(string s, string pattern)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    while (i < s.size() && j < pattern.size())
+    {
+        if (s[i] == pattern[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            j = 0;
+            k++;
+            i = k;
+        }
+    }
+    if (j == pattern.size())
+    {
+        return true;
+    }
+    return false;
+}
+vector<int> computeBuffer(string pattern)
+{
+    vector<int> lps(pattern.size());
+    int index = 0;
+    for (int i = 1; i < pattern.size();)
+    {
+        if (pattern[i] = pattern[index])
+        {
+            lps[i] = index + 1;
+            index++;
+            i++;
+        }
+        else
+        {
+            if (index)
+            {
+                index = lps[index - 1];
+            }
+            else
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
+bool KMP(string s, string pattern)
+{
+    vector<int> lps = computeBuffer(pattern);
+    int j = 0;
+    int i = 0;
+    while (i < s.size() && j < pattern.size())
+    {
+        if (s[i] == pattern[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            if (j)
+            {
+                j = lps[j - 1];
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+    if (j == pattern.size())
+    {
+        return true;
+    }
+    return false;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string s, pattern;
+    cin >> s;
+    cin >> pattern;
+    bool res = KMP(s, pattern);
+    cout << res << '\n';
+    return 0;
+}
