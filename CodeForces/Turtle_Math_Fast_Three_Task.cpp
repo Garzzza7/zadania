b8632f316b9f3427b4f04f75d07be7317add4741
Piotr Czapla <94352217+Garzzza7@users.noreply.github.com>
2024-03-10 18:00:49 +0100

    asdf

--- CodeForces/Turtle_Math_Fast_Three_Task.cpp
+++ CodeForces/Turtle_Math_Fast_Three_Task.cpp
@@ -3,17 +3,48 @@
 #include <stdlib.h>
 using namespace std;
 const long long mod = 1000000007;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(0);
-     
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        long long total = 0;
+        bool flag = false;
+
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            cin >> a;
+            total += a;
+            if (a % 3 == 1)
+            {
+                flag = true;
+            }
+        }
+        if (total % 3 == 0)
+        {
+            cout << 0 << '\n';
+        }
+        else if (total % 3 == 2)
+        {
+            cout << 1 << '\n';
+        }
+        else
+        {
+            if (flag)
+            {
+                cout << 1 << '\n';
+            }
+            else
+            {
+                cout << 2 << '\n';
+            }
+        }
+    }
     return 0;
 }
-
-
-
-
-
-
-
-
--- compc.sh
+++ compc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 g++ $1.cpp -o a.out
-./a.out < $1.txt
+time ./a.out < $1.txt
