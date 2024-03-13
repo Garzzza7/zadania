e8186dcdbf0852af3376ec3327a399987667a34c
Garzzza7 <piotr.czapela@gmail.com>
2024-03-13 22:25:00 +0100

    asdf

--- Potyczki2024/Alchemik_Bajtazar_[B].cpp
+++ Potyczki2024/Alchemik_Bajtazar_[B].cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 using namespace std;
 const long long mod = 1000000007;
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -17,26 +18,15 @@ int main()
     {
         int a, b;
         cin >> a >> b;
-
         base[a].push_back(b);
         base[b].push_back(a);
     }
     int mm;
     cin >> mm;
-    int cnt = 0;
     for (int i = 0; i < mm; i++)
     {
         int a, b;
         cin >> a >> b;
-        // if (find(base[a].begin(), base[a].end(), b) == base[a].end())
-        // {
-        //     for(auto&& aa : base[b]){
-        //         if(std::find(base[a].begin(), base[a].end(), aa) == base[a].end()) {
-        //             cout<<"+ "<<a<<' '<<aa<<'\n';
-        //             break;
-        //         }
-        //     }
-        // }
         target[a].push_back(b);
         target[b].push_back(a);
     }
@@ -68,16 +58,86 @@ int main()
         }
         cout << '\n';
     }
+    cout << "///////////////////////\n";
+    int counter = 0;
     int iter;
+    vector<string> res;
     for (iter = 1; iter <= n; iter++)
     {
         if (base[iter].size() > target[iter].size())
         {
+            for (auto &&a : base[iter])
+            {
+                if (std::find(target[iter].begin(), target[iter].end(), a) == target[iter].end())
+                {
+                    counter++;
+                    string s = "- " + to_string(iter) + " " + to_string(a);
+                    res.push_back(s);
+                    base[iter].erase(find(base[iter].begin(), base[iter].end(), a));
+                }
+            }
+            iter=1;
         }
         else if (base[iter].size() < target[iter].size())
         {
-        }
-    }
+            for (auto &&a : target[iter])
+            {
+                if (std::find(base[iter].begin(), base[iter].end(), a) == base[iter].end())
+                {
 
+                    base[iter].push_back(a);
+                    base[a].push_back(iter);
+
+                    for (auto &&b : target[a])
+                    {
+                        if (b != iter)
+                        {
+                            if (find(base[b].begin(), base[b].end(), iter) == base[b].end())
+                            {
+                                base[b].push_back(iter);
+                                sort(base[b].begin(), base[b].end());
+                                counter++;
+                                string s = "+ " + to_string(iter) + " " + to_string(b);
+                                res.push_back(s);
+                            }
+                        }
+                    }
+                    counter++;
+                    string s = "+ " + to_string(iter) + " " + to_string(a);
+                    res.push_back(s);
+                }
+            }
+            iter=1;
+        }
+        else
+        {
+            continue;
+        }
+        for (int i = 1; i < base.size(); i++)
+        {
+            cout << i << " -> ";
+            for (int j = 0; j < base[i].size(); j++)
+            {
+                cout << base[i][j] << " ";
+            }
+            cout << '\n';
+        }
+        cout << '\n';
+        for (int i = 1; i < target.size(); i++)
+        {
+            cout << i << " -> ";
+            for (int j = 0; j < target[i].size(); j++)
+            {
+                cout << target[i][j] << " ";
+            }
+            cout << '\n';
+        }
+        cout << "///////////////////////\n";
+    }
+    cout << counter << '\n';
+    for (auto &&a : res)
+    {
+        cout << a << '\n';
+    }
     return 0;
 }
