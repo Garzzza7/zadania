bb56224feecf3b06f1eb827cd646457976efb00e
Garzzza7 <piotr.czapela@gmail.com>
2024-04-07 23:09:13 +0200

    afasdf

--- Algos/Segment_Tree_Range_Sum.cpp
+++ Algos/Segment_Tree_Range_Sum.cpp
@@ -4,65 +4,82 @@
 using namespace std;
 const long long mod = 1000000007;
 vector<long long> tree;
-int tree_size;
-long long sum_query(long long start_node, long long node_left, long long node_right , long long query_left , long long query_right){
-    if(node_left>=query_left && node_right<=query_right){
+long long tree_size;
+long long sum_query(long long start_node, long long node_left, long long node_right, long long query_left, long long query_right)
+{
+    if (node_left >= query_left && node_right <= query_right)
+    {
         return tree[start_node];
     }
-    if(node_right<query_left || node_left>query_right){
+    if (node_right < query_left || node_left > query_right)
+    {
         return 0;
     }
-    long long mid = (node_left+node_right)/2;
-    return sum_query(2*start_node+1, node_left,mid, query_left,query_right)
-        +  sum_query(2*start_node+2,mid+1,node_right,query_left,query_right);
+    long long mid = (node_left + node_right) / 2;
+    long long sum = sum_query(2 * start_node, node_left, mid, query_left, query_right) + sum_query(2 * start_node + 1, mid + 1, node_right, query_left, query_right);
+    return sum;
 }
-void recursive_update(long long start_node, long long node_left, long long node_right , long long query_left , long long query_right , long long value){
-    if(node_left>=query_left && node_right<=query_right){
-        tree[start_node]=value;
+void recursive_update(long long start_node, long long node_left, long long node_right, long long query_left, long long query_right, long long value)
+{
+    if (node_left >= query_left && node_right <= query_right)
+    {
+        tree[start_node] = value;
         return;
     }
-    if(node_right<query_left || node_left>query_right){
+    if (node_right < query_left || node_left > query_right)
+    {
         return;
     }
-    long long mid = (node_left+node_right)/2;
-    recursive_update(2*start_node+1, node_left,mid, query_left,query_right,value);
-    recursive_update(2*start_node+2,mid+1,node_right,query_left,query_right,value);
-    tree[start_node] = tree[start_node * 2 + 1] + tree[start_node * 2 + 2];
+    long long mid = (node_left + node_right) / 2;
+    recursive_update(2 * start_node, node_left, mid, query_left, query_right, value);
+    recursive_update(2 * start_node + 1, mid + 1, node_right, query_left, query_right, value);
+    tree[start_node] = tree[start_node * 2] + tree[start_node * 2 + 1];
 }
-void iterative_update(long long index,long long value){
-    tree[tree_size+index]=value;
-    for(int i = (tree_size+1)/2;i>=1;i/=2){
-        tree[i]=tree[i*2+1]+tree[i*2+2];
+void iterative_update(long long index, long long value)
+{
+    tree[tree_size + index] = value;
+    for (int i = (tree_size + index) / 2; i >= 1; i /= 2)
+    {
+        tree[i] = tree[i * 2] + tree[i * 2 + 1];
     }
 }
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(0);
-    cin>>tree_size;
-    vector<long long> initial_array(tree_size,0);
-    for(int i =0 ; i<tree_size;i++){
+    cin >> tree_size;
+    vector<long long> initial_array;
+    for (int i = 0; i < tree_size; i++)
+    {
         int a;
-        cin>>a;
-        initial_array[i]=a;
-    } 
-    while(__builtin_popcount(tree_size)!=1){
+        cin >> a;
+        initial_array.push_back(a);
+    }
+    while (__builtin_popcount(tree_size) != 1)
+    {
         initial_array.push_back(0);
         tree_size++;
     }
-    tree.resize(2*tree_size);
-    for(int i = 0 ; i < tree_size;i++){
-        tree[tree_size+i]=initial_array[i];
+    tree.resize(2 * tree_size);
+    for (int i = 0; i < tree_size; i++)
+    {
+        tree[tree_size + i] = initial_array[i];
     }
-    for(int i = tree_size-1 ; i>=1 ; i--){
-        tree[i]=tree[2*i+1]+tree[2*i+2];
+    for (int i = tree_size - 1; i >= 1; i--)
+    {
+        tree[i] = tree[2 * i] + tree[2 * i + 1];
+    }
+    cout << sum_query(1, 0, tree_size - 1, 0, 7) << "\n";
+    for (auto &&a : tree)
+    {
+        cout << a << " ";
+    }
+    cout << "\n";
+    // recursive_update(1, 0, tree_size - 1, 5, 5, 100);
+    iterative_update(5, 100);
+    for (auto &&a : tree)
+    {
+        cout << a << " ";
     }
     return 0;
 }
-
-
-
-
-
-
-
-
