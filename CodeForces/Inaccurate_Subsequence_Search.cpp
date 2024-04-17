3511d0591b305ea67be32cf535697b81d155f8bd
Garzzza7 <piotr.czapela@gmail.com>
2024-04-17 21:34:24 +0200

    asd

--- CodeForces/Inaccurate_Subsequence_Search.cpp
+++ CodeForces/Inaccurate_Subsequence_Search.cpp
@@ -14,25 +14,57 @@ int main()
         int n, m, k;
         cin >> n >> m >> k;
         vector<int> a(n);
-        vector<int> b(m);
-        vector<int> c;
+
+        multiset<int> b;
+        multiset<int> done;
+        multiset<int> remain;
         for (int i = 0; i < n; i++)
         {
             cin >> a[i];
         }
         for (int i = 0; i < m; i++)
         {
-            cin >> b[i];
+            int bb;
+            cin >> bb;
+            b.insert(bb);
         }
-        set<int> found;
-        set<int> notfound;
-        for(int i = 0 ; i < m ; ; i++){
-            for(int j = 0 ; j < n ; i++){
-               if(b[i]==a[j] ){
-                    found.insert(b[j]);
-               } 
+        for (int i = 0; i < m; i++)
+        {
+            if (b.find(a[i]) != b.end())
+            {
+                b.erase(b.find(a[i]));
+                done.insert(a[i]);
+            }
+            else
+            {
+                remain.insert(a[i]);
             }
         }
+        int ans = (done.size() >= k);
+        for (int l = 0; l < n - m; l++)
+        {
+            int r = l + m - 1;
+            if (remain.find(l) != remain.end())
+            {
+                remain.erase(remain.find(l));
+            }
+            else if (done.find(l) != done.end())
+            {
+                done.erase(done.find(l));
+                b.insert(l);
+            }
+            if (b.find(a[r]) != b.end())
+            {
+                b.erase(b.find(a[r]));
+                done.insert(a[r]);
+            }
+            else
+            {
+                remain.insert(a[r]);
+            }
+            ans += (done.size() >= k);
+        }
+        cout << ans << '\n';
     }
     return 0;
 }
