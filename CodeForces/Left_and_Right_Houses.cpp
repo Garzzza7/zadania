85adec481588d36414407aec96ad72eb4b7f92b3
Garzzza7 <piotr.czapela@gmail.com>
2024-04-19 01:05:17 +0200

    left and right

--- CodeForces/Left_and_Right_Houses.cpp
+++ CodeForces/Left_and_Right_Houses.cpp
@@ -1,26 +1,53 @@
 #include <bits/stdc++.h>
 #include <cmath>
+#include <cstdint>
 #include <stdlib.h>
+#include <vector>
 using namespace std;
 const long long mod = 1000000007;
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<int> vec;
-        for (int i 0; i < n; i++)
-        {
-            int a;
-            cin >> a;
-            vec.push_back(a);
-        }
-
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> vec;
+    int cntZeros = 0;
+    int cntOnes = 0;
+    for (int i = 0; i < n; i++) {
+      int a;
+      cin >> a;
+      vec.push_back(a);
+      if (a == 0) {
+        cntZeros++;
+      } else {
+        cntOnes++;
+        //asdfassadsdsafdsdafsdfs
+      }
     }
-    return 0;
+    int cnt = 0;
+    int res1 = INT32_MAX;
+    int res2 = INT32_MAX;
+
+    vector<int> ans;
+    for (int l = 0; l < vec.size(); l++) {
+      if (cnt >= round(l / 2) && cntOnes >= round((n - l) / 2) &&
+          cntZeros <= round((n - l) / 2)) {
+        ans.push_back(l);
+        cout << l << "\n";
+        break;
+      }
+      if (vec[l] == 0) {
+        cnt++;
+        cntZeros--;
+      } else {
+        cntOnes--;
+      }
+    }
+    sort(ans.begin(), ans.end());
+    // cout << "SIZE = " << ans.size() << "\n";
+  }
+  return 0;
 }
--- CodeForces/Nene_and_the_Card_Game.cpp
+++ CodeForces/Nene_and_the_Card_Game.cpp
@@ -3,34 +3,30 @@
 #include <stdlib.h>
 using namespace std;
 const long long mod = 1000000007;
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<int> vec;
-        for (int i = 0; i < n; i++)
-        {
-            int a;
-            cin >> a;
-            vec.push_back(a);
-        }
-        sort(vec.begin(), vec.end());
-        int cnt = 0;
-        for (int i = 0; i < vec.size() - 1; i++)
-        {
-            if (vec[i] == vec[i + 1])
-            {
-                cnt++;
-            }
-        }
-        // cnt += (n - (2 * cnt)) / 2;
-        cout << cnt << '\n';
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> vec;
+    for (int i = 0; i < n; i++) {
+
+      int a;
+      cin >> a;
+      vec.push_back(a);
     }
-    return 0;
+    sort(vec.begin(), vec.end());
+    int cnt = 0;
+    for (int i = 0; i < vec.size() - 1; i++) {
+      if (vec[i] == vec[i + 1]) {
+        cnt++;
+      }
+    }
+    // cnt += (n - (2 * cnt)) / 2;
+    cout << cnt << '\n';
+  }
+  return 0;
 }
