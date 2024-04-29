3916fe00349d408a5a86ddf1ecf73556be3bb96e
Garzzza7 <piotr.czapela@gmail.com>
2024-04-29 23:32:10 +0200

    exe

---
+++ CodeForces/Minimizing_the_Sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+const long long mod = 1000000007;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> vec(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> vec[i];
+        }
+
+    }
+    return 0;
+}
