9b6c58c27a45a60e0726f40237f42534388e6406
Piotr Czapla <piotr.czapela@gmail.com>
2024-05-09 20:33:11 +0200

    exe

---
+++ CodeForces/Coin_Games.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+const long long mod = 1000000007;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+      char c;
+      cin >> c;
+      if (c == 'U')
+      {
+        cnt++;
+      }
+    }
+    if (cnt % 2 != 0)
+    {
+      cout << "YES\n";
+    }
+    else
+    {
+      cout << "NO\n";
+    }
+  }
+  return 0;
+}
