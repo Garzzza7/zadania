e07fe50f21f7d38230dabe2d8f3d590047ae059a
Piotr Czapla <piotr.czapela@gmail.com>
2024-05-09 16:21:23 +0200

    webscraping + ignore + installation scirpt

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+install_debian() {
+    sudo apt update
+    sudo apt install -y python3 g++ python3-pip
+    python -m pip install beautifulsoup4
+    python -m pip install requests    
+}
+
+install_arch() {
+    sudo pacman -Syu --noconfirm python3 g++ python3-pip
+    python -m pip install beautifulsoup4
+    python -m pip install requests    
+}
+
+install_mingw64() {
+    pacman -Syu --noconfirm python3 g++ python3-pip
+    python -m pip install beautifulsoup4
+    python -m pip install requests    
+}
+
+if [ -f /etc/debian_version ]; then
+    echo "Detected Debian-based OS."
+    install_debian
+elif [ -f /etc/arch-release ]; then
+    echo "Detected Arch Linux."
+    install_arch
+elif [ "$(uname -o)" = "MINGW64" -o "$(uname -o)" = "Msys" ]; then
+    echo "Detected MINGW64/MSYS2."
+    install_mingw64
+else
+    echo "Unknown system"
+fi
---
+++ net.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def remove_until_dot(text):
+    dot_index = text.find('.')
+    if dot_index != -1:
+        return text[dot_index + 1:].strip()
+    else:
+        return text
+
+
+separators = ["?","!","'",":",";","\"","\\"]
+url = input("URL: ")
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+title_divs = soup.find("div", class_="title")
+
+examples = soup.find("pre")
+exe = examples.find_all()
+title = remove_until_dot(title_divs.get_text())
+
+title = title.replace(" ","_")
+
+for sep in separators:
+    title = title.replace(sep,"")
+
+temp = open("TEMPLATE.cpp", "r")
+template = temp.read()
+
+codefile = open(title+".cpp","w")
+txtfile = open(title+".txt","w")
+
+codefile.write(template)
+
+for a in exe:
+    txtfile.write(a.get_text()+"\n")
+
+print("Done!\n")
