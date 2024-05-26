9b77c8045561a71bf7d2f1363158205b71b97a7f
Garzzza7 <piotr.czapela@gmail.com>
2024-05-26 12:46:03 +0200

    template update

--- SPOJ/TEMPLATE.cpp
+++ SPOJ/TEMPLATE.cpp
@@ -9,12 +9,12 @@
   cout << "\n";
 #define help ios::sync_with_stdio(false)
 #define me cin.tie(0)
-#define sortasc(vec) sort(vec.begin(), vec.end())
-#define sortdes(vec) sort(vec.end(), vec.begin())
-#define rev(vec) reverse(vec.begin(), vec.end())
-#define setasc(vec) set<int, greater<int>> vec
-#define sortpairasc(vec) sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second < right.second; })
-#define sortpairdec(vec) sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second > right.second; })
+#define sortasc(vec) std::sort(vec.begin(), vec.end())
+#define sortdes(vec)  std::sort(vec.begin(), vec.end(), std::greater<>())
+#define rev(vec) std::reverse(vec.begin(), vec.end())
+#define setasc(vec) std::set<int, std::greater<int>> vec
+#define sortpairasc(vec) std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second < right.second; })
+#define sortpairdec(vec) std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second > right.second; })
 using namespace std;
 const long long mod = 1000000007;
 int main()
