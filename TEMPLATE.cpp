f243b6986f5b1ffa3b4db588020c95518191f131
Piotr Czapla <piotr.czapela@gmail.com>
2024-07-12 10:40:27 +0200

    template update

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
 #include <cmath>
 #include <stdlib.h>
-#define printarr(arr)    \
-  for (auto &&a : (arr)) \
-{                      \
-  cout << a << " ";    \
-}                      \
-cout << "\n";
+#define print_rvalues(arr) \
+  for (auto &&a : (arr))   \
+  {                        \
+    cout << a << " ";      \
+  }                        \
+  cout << "\n";
+#define print_lvalues(arr)    \
+  for (const auto &a : (arr)) \
+  {                           \
+    cout << a << " ";         \
+  }                           \
+  cout << "\n";
 #define help ios::sync_with_stdio(false)
 #define me cin.tie(0)
 #define sortasc(vec) std::sort(vec.begin(), vec.end())
@@ -15,6 +21,11 @@ cout << "\n";
 #define setasc(vec) std::set<int, std::greater<int>> vec
 #define sortpairasc(vec) std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second < right.second; })
 #define sortpairdec(vec) std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second > right.second; })
+#define swpint(a, b) \
+  a ^= b;            \
+  b ^= a;            \
+  a ^= b;
+#define LSB(a) a & -a
 #define MOD 1000000007
 
 using namespace std;
@@ -24,7 +35,7 @@ int main()
   help;
   me;
   int t;
-  scanf("%d",&t);
+  cin >> t;
   while (t--)
   {
   }
