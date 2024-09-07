887f315518457d4ab836287576102c732df4d6a6
Garzzza7 <piotr.czapela@gmail.com>
2024-09-07 15:14:08 +0200

    template update

--- SPOJ/TEMPLATE.cpp
+++ SPOJ/TEMPLATE.cpp
@@ -1,37 +1,39 @@
 #include <bits/stdc++.h>
 #include <cmath>
 #include <stdlib.h>
-#define print_rvalues(vec) \
-  for (auto &&a : (vec))   \
-  {                        \
-    cout << a << " ";      \
-  }                        \
-  cout << "\n";
-#define print_lvalues(vec)    \
-  for (const auto &a : (vec)) \
-  {                           \
-    cout << a << " ";         \
-  }                           \
-  cout << "\n";
+#define print_rvalues(vec)                                                     \
+  for (auto &&a : (vec)) {                                                     \
+    std::cout << a << ' ';                                                     \
+  }                                                                            \
+  std::cout << '\n';
+#define print_lvalues(vec)                                                     \
+  for (const auto &a : (vec)) {                                                \
+    std::cout << a << ' ';                                                     \
+  }                                                                            \
+  std::cout << '\n';
 #define help ios::sync_with_stdio(false)
 #define me cin.tie(0)
 #define sortasc(vec) std::sort(vec.begin(), vec.end())
 #define sortdes(vec) std::sort(vec.begin(), vec.end(), std::greater<>())
 #define rev(vec) std::reverse(vec.begin(), vec.end())
 #define setasc(vec) std::set<int, std::greater<int>> vec
-#define sortpairascS(vec) \
-  std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second < right.second; })
-#define sortpairdecS(vec) \
-  std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second > right.second; })
-#define sortpairascF(vec)           \
-  std::sort(vec.begin(), vec.end(), \
+#define sortpairascS(vec)                                                      \
+  std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) {              \
+    return left.second < right.second;                                         \
+  })
+#define sortpairdesS(vec)                                                      \
+  std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) {              \
+    return left.second > right.second;                                         \
+  })
+#define sortpairascF(vec)                                                      \
+  std::sort(vec.begin(), vec.end(),                                            \
             [](auto &left, auto &right) { return left.first < right.first; })
-#define sortpairdecF(vec)           \
-  std::sort(vec.begin(), vec.end(), \
+#define sortpairdesF(vec)                                                      \
+  std::sort(vec.begin(), vec.end(),                                            \
             [](auto &left, auto &right) { return left.first > right.first; })
-#define swpint(a, b) \
-  a ^= b;            \
-  b ^= a;            \
+#define swpint(a, b)                                                           \
+  a ^= b;                                                                      \
+  b ^= a;                                                                      \
   a ^= b;
 #define LSB(a) a & -a
 #define MOD 1000000007
@@ -41,8 +43,21 @@
 
 using namespace std;
 
-int main()
-{
+template <typename T_vector>
+void printarr(const T_vector &v, bool inc = 0, int begin = -1, int end = -1) {
+  if (begin < 0) {
+    begin ^= begin;
+  }
+  if (end < 0) {
+    end = int(v.size());
+  }
+
+  for (int i = begin; i < end; i++) {
+    std::cout << v[i] + (inc ? 1 : 0) << (i < end - 1 ? ' ' : '\n');
+  }
+}
+
+int main() {
 #if TIME
   auto begin = std::chrono::high_resolution_clock::now();
 #endif
@@ -52,16 +67,18 @@ int main()
   me;
 #endif
 
-  int t;
-  cin >> t;
-  while (t--)
-  {
+  int T;
+  cin >> T;
+  while (T--) {
   }
 
 #if TIME
   auto end = std::chrono::high_resolution_clock::now();
   cout << setprecision(4) << fixed;
-  cout << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds\n";
+  cout << "Execution time: "
+       << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin)
+              .count()
+       << " seconds\n";
 #endif
   return 0;
 }
