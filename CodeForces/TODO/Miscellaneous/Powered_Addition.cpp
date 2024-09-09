b5c0d4c43c96d3bb1c0125b585eeb0036a0c752a
Garzzza7 <piotr.czapela@gmail.com>
2024-09-09 15:53:36 +0200

    exes

---
+++ CodeForces/TODO/Miscellaneous/Powered_Addition.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <stdlib.h>
+#define print_rvalues(vec) \
+  for (auto &&a : (vec))   \
+  {                        \
+    std::cout << a << ' '; \
+  }                        \
+  std::cout << '\n';
+#define print_lvalues(vec)    \
+  for (const auto &a : (vec)) \
+  {                           \
+    std::cout << a << ' ';    \
+  }                           \
+  std::cout << '\n';
+#define help ios::sync_with_stdio(false)
+#define me cin.tie(0)
+#define sortasc(vec) std::sort(vec.begin(), vec.end())
+#define sortdes(vec) std::sort(vec.begin(), vec.end(), std::greater<>())
+#define rev(vec) std::reverse(vec.begin(), vec.end())
+#define setasc(vec) std::set<int, std::greater<int>> vec
+#define sortpairascS(vec) \
+  std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second < right.second; })
+#define sortpairdesS(vec) \
+  std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second > right.second; })
+#define sortpairascF(vec)           \
+  std::sort(vec.begin(), vec.end(), \
+            [](auto &left, auto &right) { return left.first < right.first; })
+#define sortpairdesF(vec)           \
+  std::sort(vec.begin(), vec.end(), \
+            [](auto &left, auto &right) { return left.first > right.first; })
+#define swpint(a, b) \
+  a ^= b;            \
+  b ^= a;            \
+  a ^= b;
+#define LSB(a) a & -a
+#define MOD 1000000007
+#define DEBUG 0
+#define FAST 1
+#define TIME 0
+
+using namespace std;
+
+template <typename T_vector>
+void printarr(const T_vector &v, bool inc = 0, int begin = -1, int end = -1)
+{
+  if (begin < 0)
+  {
+    begin ^= begin;
+  }
+  if (end < 0)
+  {
+    end = int(v.size());
+  }
+
+  for (int i = begin; i < end; i++)
+  {
+    std::cout << v[i] + (inc ? 1 : 0) << (i < end - 1 ? ' ' : '\n');
+  }
+}
+
+int main()
+{
+#if TIME
+  auto begin = std::chrono::high_resolution_clock::now();
+#endif
+
+#if FAST
+  help;
+  me;
+#endif
+
+  int T;
+  cin >> T;
+  while (T--)
+  {
+    int N;
+    cin >> N;
+    int maxi = INT32_MIN;
+    int maxdiff = 0;
+
+    int last = INT32_MAX;
+    bool flag = 0;
+    for (int i = 0; i < N; i++)
+    {
+      int aa;
+      cin >> aa;
+      if (aa < last && i != 0)
+      {
+        flag = 1;
+      }
+      maxi = max(maxi, aa);
+      maxdiff = max(maxdiff, abs(maxi - aa));
+      last = aa;
+    }
+
+    int iter = 0;
+    if (flag)
+    {
+      int res = 0;
+      while (res < maxdiff)
+      {
+        res += 1 << iter;
+        iter++;
+      }
+    }
+    cout << iter << "\n";
+  }
+
+#if TIME
+  auto end = std::chrono::high_resolution_clock::now();
+  cout << setprecision(4) << fixed;
+  cout << "Execution time: "
+       << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin)
+              .count()
+       << " seconds\n";
+#endif
+  return 0;
+}
---
+++ CodeForces/TODO/Miscellaneous/Tetrahedron.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <stdlib.h>
+#define print_rvalues(vec) \
+  for (auto &&a : (vec))   \
+  {                        \
+    std::cout << a << ' '; \
+  }                        \
+  std::cout << '\n';
+#define print_lvalues(vec)    \
+  for (const auto &a : (vec)) \
+  {                           \
+    std::cout << a << ' ';    \
+  }                           \
+  std::cout << '\n';
+#define help ios::sync_with_stdio(false)
+#define me cin.tie(0)
+#define sortasc(vec) std::sort(vec.begin(), vec.end())
+#define sortdes(vec) std::sort(vec.begin(), vec.end(), std::greater<>())
+#define rev(vec) std::reverse(vec.begin(), vec.end())
+#define setasc(vec) std::set<int, std::greater<int>> vec
+#define sortpairascS(vec) \
+  std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second < right.second; })
+#define sortpairdesS(vec) \
+  std::sort(vec.begin(), vec.end(), [](auto &left, auto &right) { return left.second > right.second; })
+#define sortpairascF(vec)           \
+  std::sort(vec.begin(), vec.end(), \
+            [](auto &left, auto &right) { return left.first < right.first; })
+#define sortpairdesF(vec)           \
+  std::sort(vec.begin(), vec.end(), \
+            [](auto &left, auto &right) { return left.first > right.first; })
+#define swpint(a, b) \
+  a ^= b;            \
+  b ^= a;            \
+  a ^= b;
+#define LSB(a) a & -a
+#define MOD 1000000007
+#define DEBUG 0
+#define FAST 1
+#define TIME 0
+
+using namespace std;
+
+template <typename T_vector>
+void printarr(const T_vector &v, bool inc = 0, int begin = -1, int end = -1)
+{
+  if (begin < 0)
+  {
+    begin ^= begin;
+  }
+  if (end < 0)
+  {
+    end = int(v.size());
+  }
+
+  for (int i = begin; i < end; i++)
+  {
+    std::cout << v[i] + (inc ? 1 : 0) << (i < end - 1 ? ' ' : '\n');
+  }
+}
+
+int main()
+{
+#if TIME
+  auto begin = std::chrono::high_resolution_clock::now();
+#endif
+
+#if FAST
+  help;
+  me;
+#endif
+
+  int N;
+  cin >> N;
+  int cntD = 1;
+  int cntabc = 0;
+  for (int i = 0; i < N; i++)
+  {
+    int d = cntabc * 3 % MOD;
+    int abc = (cntabc * 2 + cntD) % MOD;
+    cntD = d;
+    cntabc = abc;
+  }
+  cout << cntD << "\n";
+
+#if TIME
+  auto end = std::chrono::high_resolution_clock::now();
+  cout << setprecision(4) << fixed;
+  cout << "Execution time: "
+       << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin)
+              .count()
+       << " seconds\n";
+#endif
+  return 0;
+}
