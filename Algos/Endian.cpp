2790beeb865d692217101ff9f118c275758bce64
Garzzza7 <piotr.czapela@gmail.com>
2025-04-12 23:55:27 +0200

    modify Endian

--- Algos/Endian.cpp
+++ Algos/Endian.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 
-char *find_little_endian(std::string word) {
+char *find_little_endian(std::string &word) {
     size_t word_len = word.size();
     char *little_endian = (char *) malloc((2 * word_len + 1) * sizeof(char));
 
-    for (size_t i = word_len; i-- > 0;) {
+    for (size_t i = word_len; i > 0; i--) {
 	snprintf(&little_endian[(word_len - 1 - i) * 2], 3, "%02X",
 		 (unsigned char) word[i]);
     }
@@ -14,7 +14,7 @@ char *find_little_endian(std::string word) {
     return little_endian;
 }
 
-char *find_big_endian(std::string word) {
+char *find_big_endian(std::string &word) {
     size_t length = word.size();
     char *big_endian = (char *) malloc((2 * length + 1) * sizeof(char));
 
@@ -27,10 +27,14 @@ char *find_big_endian(std::string word) {
 }
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
     std::string word;
     std::cin >> word;
-    auto le = find_little_endian(word);
-    auto be = find_big_endian(word);
-    std::cout << le << " " << be << "\n";
+    char *le = find_little_endian(word);
+    char *be = find_big_endian(word);
+    std::cout << "Little Endian: " << le << "\nBig Endian: " << be << "\n";
     return 0;
 }
