84b2db76eb77a69e548e07109a41d9ddeeec7012
Garzzza7 <piotr.czapela@gmail.com>
2025-06-12 02:58:44 +0200

    fix endian

--- Algos/Endian.cpp
+++ Algos/Endian.cpp
@@ -1,36 +1,23 @@
+#include <iomanip>
 #include <iostream>
 #include <string>
 
-// while using g++ sometimes it fails, idk why. UB?
-
 std::string find_little_endian(const std::string &word) {
-    const size_t word_len = word.size();
-    auto little_endian =
-	static_cast<char *>(malloc((2 * word_len + 1) * sizeof(char)));
-
-    for (size_t i = word_len; i > 0; i--) {
-	snprintf(&little_endian[(word_len - 1 - i) * 2], 3, "%02X",
-		 static_cast<unsigned char>(word[i]));
+    std::ostringstream oss;
+    for (size_t i = word.size() - 1; i > 0; i--) {
+	oss << std::uppercase << std::setfill('0') << std::setw(2) << std::hex
+	    << static_cast<unsigned int>(static_cast<unsigned char>(word[i]));
     }
-
-    little_endian[2 * word_len] = '\0';
-    std::string res(little_endian);
-    return res;
+    return oss.str();
 }
 
 std::string find_big_endian(const std::string &word) {
-    const size_t length = word.size();
-    auto big_endian =
-	static_cast<char *>(malloc((2 * length + 1) * sizeof(char)));
-
-    for (size_t i = 0; i < length; i++) {
-	snprintf(&big_endian[i * 2], 3, "%02X",
-		 static_cast<unsigned char>(word[i]));
+    std::ostringstream oss;
+    for (auto &&i : word) {
+	oss << std::uppercase << std::setfill('0') << std::setw(2) << std::hex
+	    << static_cast<unsigned int>(i);
     }
-
-    big_endian[2 * length] = '\0';
-    std::string res(big_endian);
-    return res;
+    return oss.str();
 }
 
 int main() {
@@ -42,6 +29,6 @@ int main() {
     std::cin >> word;
     const auto le = find_little_endian(word);
     const auto be = find_big_endian(word);
-    std::cout << "Little Endian: " << le << "\nBig Endian: " << be << "\n";
+    std::cout << "Little Endian: " << le << "\nBig Endian: " << be << '\n';
     return 0;
 }
