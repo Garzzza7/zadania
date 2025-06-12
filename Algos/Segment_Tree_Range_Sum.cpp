d7873e28a0267a1a4c767c5074798cd8f1ac2a1c
Garzzza7 <piotr.czapela@gmail.com>
2025-06-12 04:10:41 +0200

    new segtrees

--- Algos/Segment_Tree_Range_Sum.cpp
+++ Algos/Segment_Tree_Range_Sum.cpp
@@ -1,5 +1,201 @@
+#include <cstdint>
 #include <iostream>
 #include <vector>
+
+template <typename T = int>
+struct ram_sum_seg_tree {
+    int size{1};
+    std::vector<long long> sums;
+
+    ram_sum_seg_tree(int _n) {
+	while (size < _n) {
+	    size <<= 1;
+	}
+	sums.assign(2 * size, 0ll);
+    }
+
+    void build(std::vector<int>& vec, int x, int lx, int rx) {
+	if (rx - lx == 1) {
+	    if (lx < (int) vec.size()) {
+		sums[x] = vec[lx];
+	    }
+	    return;
+	}
+	const int mid = (lx + rx) / 2;
+	build(vec, 2 * x + 1, lx, mid);
+	build(vec, 2 * x + 2, mid, rx);
+	sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
+    }
+
+    void build(std::vector<int>& vec) {
+	build(vec, 0, 0, size);
+    }
+
+    void set(int i, int v, int x, int lx, int rx) {
+	if (rx - lx == 1) {
+	    sums[x] = v;
+	    return;
+	}
+	const int mid = (lx + rx) / 2;
+	if (i < mid) {
+	    set(i, v, 2 * x + 1, lx, mid);
+	} else {
+	    set(i, v, 2 * x + 2, mid, rx);
+	}
+	sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
+    }
+
+    void set(int i, int v) {
+	set(i, v, 0, 0, size);
+    }
+
+    long long sum(int l, int r, int x, int lx, int rx) {
+	if (lx >= r || l >= rx) {
+	    return 0ll;
+	}
+	if (lx >= r && rx <= r) {
+	    return sums[x];
+	}
+	const int mid = (lx + rx) / 2;
+	long long s1 = sum(l, r, 2 * x + 1, lx, mid);
+	long long s2 = sum(l, r, 2 * x + 2, mid, rx);
+	return s1 + s2;
+    }
+
+    long long sum(int l, int r) {
+	return sum(l, r, 0, 0, size);
+    }
+};
+
+template <typename T = int>
+struct ram_max_seg_tree {
+    int size{1};
+    std::vector<long long> maxs;
+
+    ram_max_seg_tree(int _n) {
+	while (size < _n) {
+	    size <<= 1;
+	}
+	maxs.assign(2 * size, 0ll);
+    }
+
+    void build(std::vector<int>& vec, int x, int lx, int rx) {
+	if (rx - lx == 1) {
+	    if (lx < (int) vec.size()) {
+		maxs[x] = vec[lx];
+	    }
+	    return;
+	}
+	const int mid = (lx + rx) / 2;
+	build(vec, 2 * x + 1, lx, mid);
+	build(vec, 2 * x + 2, mid, rx);
+	maxs[x] = std::max(maxs[2 * x + 1], maxs[2 * x + 2]);
+    }
+
+    void build(std::vector<int>& vec) {
+	build(vec, 0, 0, size);
+    }
+
+    void set(int i, int v, int x, int lx, int rx) {
+	if (rx - lx == 1) {
+	    maxs[x] = v;
+	    return;
+	}
+	const int mid = (lx + rx) / 2;
+	if (i < mid) {
+	    set(i, v, 2 * x + 1, lx, mid);
+	} else {
+	    set(i, v, 2 * x + 2, mid, rx);
+	}
+	maxs[x] = std::max(maxs[2 * x + 1], maxs[2 * x + 2]);
+    }
+
+    void set(int i, int v) {
+	set(i, v, 0, 0, size);
+    }
+
+    int max(int l, int r, int x, int lx, int rx) {
+	if (lx >= r || l >= rx) {
+	    return INT32_MIN;
+	}
+	if (lx >= r && rx <= r) {
+	    return maxs[x];
+	}
+	const int mid = (lx + rx) / 2;
+	int max1 = max(l, r, 2 * x + 1, lx, mid);
+	int max2 = max(l, r, 2 * x + 2, mid, rx);
+	return std::max(max1, max2);
+    }
+
+    int max(int l, int r) {
+	return max(l, r, 0, 0, size);
+    }
+};
+
+template <typename T = int>
+struct ram_min_seg_tree {
+    int size{1};
+    std::vector<long long> mins;
+
+    ram_min_seg_tree(int _n) {
+	while (size < _n) {
+	    size <<= 1;
+	}
+	mins.assign(2 * size, 0ll);
+    }
+
+    void build(std::vector<int>& vec, int x, int lx, int rx) {
+	if (rx - lx == 1) {
+	    if (lx < (int) vec.size()) {
+		mins[x] = vec[lx];
+	    }
+	    return;
+	}
+	const int mid = (lx + rx) / 2;
+	build(vec, 2 * x + 1, lx, mid);
+	build(vec, 2 * x + 2, mid, rx);
+	mins[x] = std::min(mins[2 * x + 1], mins[2 * x + 2]);
+    }
+
+    void build(std::vector<int>& vec) {
+	build(vec, 0, 0, size);
+    }
+
+    void set(int i, int v, int x, int lx, int rx) {
+	if (rx - lx == 1) {
+	    mins[x] = v;
+	    return;
+	}
+	const int mid = (lx + rx) / 2;
+	if (i < mid) {
+	    set(i, v, 2 * x + 1, lx, mid);
+	} else {
+	    set(i, v, 2 * x + 2, mid, rx);
+	}
+	mins[x] = std::min(mins[2 * x + 1], mins[2 * x + 2]);
+    }
+
+    void set(int i, int v) {
+	set(i, v, 0, 0, size);
+    }
+
+    int min(int l, int r, int x, int lx, int rx) {
+	if (lx >= r || l >= rx) {
+	    return INT32_MAX;
+	}
+	if (lx >= r && rx <= r) {
+	    return mins[x];
+	}
+	const int mid = (lx + rx) / 2;
+	int min1 = min(l, r, 2 * x + 1, lx, mid);
+	int min2 = min(l, r, 2 * x + 2, mid, rx);
+	return std::min(min1, min2);
+    }
+
+    int min(int l, int r) {
+	return min(l, r, 0, 0, size);
+    }
+};
 
 std::vector<long long> tree;
 int tree_size;
@@ -66,7 +262,7 @@ int main() {
 	tree[i] = tree[2 * i] + tree[2 * i + 1];
     }
 
-    for (const auto &a : tree) {
+    for (const auto& a : tree) {
 	std::cout << a << " ";
     }
     // std::cout << sum_query(1, 0, tree_size - 1, 0, 7) << "\n";
