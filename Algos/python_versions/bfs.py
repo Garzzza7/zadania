cda824f343a5cfa2cff993a70e880f117b1ffa2b
Garzzza7 <piotr.czapela@gmail.com>
2025-06-30 00:12:28 +0200

    python bfs

---
+++ Algos/python_versions/bfs.py
@@ -0,0 +1,41 @@
+def bfs(ver, adj, visited, q):
+    if visited[ver]:
+        return
+    visited[ver] = True
+    print(ver, end=" ")
+    for v in adj[ver]:
+        q.append(v)
+    while len(q) > 0:
+        bfs(q[0], adj, visited, q)
+        if len(q) > 0:
+            q.pop(0)
+
+
+def iter_bfs(ver, adj, visited):
+    q = []
+    q.append(ver)
+    while len(q) > 0:
+        curr = q[0]
+        q.pop(0)
+        visited[curr] = True
+        print(curr, end=" ")
+        for v in adj[curr]:
+            if visited[v] == False:
+                q.append(v)
+
+
+v = int(input())
+visited = [False] * (v + 1)
+adj = [[] for _ in range(v + 1)]
+edges = int(input())
+for i in range(edges):
+    inp = input().split()
+    x = int(inp[0])
+    y = int(inp[1])
+    adj[x].append(y)
+
+q = []
+bfs(1, adj, visited, q)
+print()
+visited = [False] * (v + 1)
+iter_bfs(1, adj, visited)
