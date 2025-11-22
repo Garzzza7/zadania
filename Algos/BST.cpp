d46891fcdc46df5c01e6957e4dcac699c8947dd7
Garzzza7 <piotr.czapela@gmail.com>
2025-11-22 22:56:04 +0100

    changes

--- Algos/BST.cpp
+++ Algos/BST.cpp
@@ -12,8 +12,7 @@ template <typename T = int> struct node {
     node(T v) : val(v) {
     }
 
-    ~node() {
-    }
+    ~node() = default;
 
     bool
     operator<=(const node<T> &p) const {
@@ -26,15 +25,17 @@ template <typename T = int> struct node {
     }
 };
 
+// create on stack , add elements from heap
+
 template <typename T = int> struct bst {
     node<T> *root;
-    node<T> *sentinel;
+    // node<T> *sentinel;
     std::vector<node<T>> nodes;
 
     bst() {
 	root = new node();
-	sentinel = new node(-2137);
-	root->p = sentinel;
+	// sentinel = new node(-2137);
+	// root->p = sentinel;
     }
 
     bst(T v) {
@@ -67,14 +68,14 @@ template <typename T = int> struct bst {
     void
     insert(node<T> *n, node<T> *curr) {
 	if (*n <= *curr) {
-	    if (curr->l) {
+	    if (curr->l != nullptr) {
 		insert(n, curr->l);
 	    } else {
 		curr->l = n;
 		n->p = curr;
 	    }
 	} else {
-	    if (curr->r) {
+	    if (curr->r != nullptr) {
 		insert(n, curr->r);
 	    } else {
 		curr->r = n;
@@ -94,26 +95,106 @@ template <typename T = int> struct bst {
 	    return;
 	}
 	if (n->val == curr->val) {
-                // TODO: finish
+	    auto is_left = [](node<T> *n) -> bool {
+		// true -> l , false -> r
+		if (n->p->l == n) {
+		    return true;
+		}
+		return false;
+	    };
 	    if (curr->l == nullptr and curr->r == nullptr) {
-                    curr->p = nullptr;
-                    delete curr;
+		// leaf
+		if (is_left(curr)) {
+		    curr->p->l = nullptr;
+		} else {
+		    curr->p->r = nullptr;
+		}
+		delete curr;
 	    } else if (curr->l == nullptr and curr->r != nullptr) {
-                   // curr->p->r = curr->r;
-                   // curr->r->p = curr->p;
-                   // delete curr;
+		// has one child r
+		if (is_left(curr)) {
+		    curr->p->l = curr->r;
+		} else {
+		    curr->p->r = curr->r;
+		}
+		delete curr;
 	    } else if (curr->l != nullptr and curr->r == nullptr) {
-                   // curr->p->l = curr->l;
-                   // curr->l->p = curr->p;
-                   // delete curr;
-            } else {
+		// has one child l
+		if (is_left(curr)) {
+		    curr->p->l = curr->l;
+		} else {
+		    curr->p->r = curr->l;
+		}
+		delete curr;
+	    } else {
+		// has both children
+		node<T> *succ = find_successor(curr);
+		if (succ != nullptr) {
+		    T buff = succ->val;
+		    remove(succ, curr);
+		    curr->val = buff;
+		} else {
+		    node<T> *pred = find_predecessor(curr);
+		    if (pred != nullptr) {
+			T buff = pred->val;
+			remove(pred, curr);
+			curr->val = buff;
+		    }
+		}
 	    }
-            delete curr;
-	}
-	if (n->val < curr->val) {
+	} else if (n->val <= curr->val) {
 	    return remove(n, curr->l);
+	} else {
+	    return remove(n, curr->r);
 	}
-	return remove(n, curr->r);
+    }
+
+    node<T> *
+    find_predecessor(node<T> *n) {
+	if (n->l != nullptr) {
+	    node<T> *curr = n->l;
+	    while (curr->r != nullptr) {
+		curr = curr->r;
+	    }
+	    return curr;
+	}
+	node<T> *curr = n->p;
+	node<T> *buff = n;
+	while (curr != nullptr and buff == curr->l) {
+	    buff = curr;
+	    curr = curr->p;
+	}
+	return curr;
+    }
+
+    node<T> *
+    find_successor(node<T> *n) {
+	if (n->r != nullptr) {
+	    node<T> *curr = n->r;
+	    while (curr->l != nullptr) {
+		curr = curr->l;
+	    }
+	    return curr;
+	}
+	node<T> *curr = n->p;
+	node<T> *buff = n;
+	while (curr != nullptr and buff == curr->r) {
+	    buff = curr;
+	    curr = curr->p;
+	}
+	return curr;
+    }
+
+    void
+    l_rotate(node<T> *n) {
+	(void) n;
+	// TODO: finish
+    }
+
+    void
+    r_rotate(node<T> *n) {
+	(void) n;
+	// TODO: finish
     }
 
     bool
@@ -135,36 +216,109 @@ template <typename T = int> struct bst {
 	return find(n, curr->r);
     }
 
-    void 
-    dfs() {
-            dfs(root);
+    bool
+    find(const T &n) {
+	return find(n, root);
     }
 
-    void 
-    dfs(node<T>* n) {
-          std::cout << n->val << "\n";
-          if(n->l != nullptr){
-                dfs(n->l);
-          }
-          if(n->r != nullptr){
-                  dfs(n->r);
-          }
+    bool
+    find(const T &n, node<T> *curr) {
+	if (curr == nullptr) {
+	    return false;
+	}
+	if (n == curr->val) {
+	    return true;
+	}
+	if (n < curr->val) {
+	    return find(n, curr->l);
+	}
+	return find(n, curr->r);
+    }
+
+    void
+    pre_order() {
+	pre_order(root);
+    }
+
+    void
+    pre_order(node<T> *n) {
+	std::cout << n->val << "\n";
+	if (n->l != nullptr) {
+	    pre_order(n->l);
+	}
+	if (n->r != nullptr) {
+	    pre_order(n->r);
+	}
+    }
+
+    void
+    in_order() {
+	in_order(root);
+    }
+
+    void
+    in_order(node<T> *n) {
+	if (n->l != nullptr) {
+	    in_order(n->l);
+	}
+	std::cout << n->val << "\n";
+	if (n->r != nullptr) {
+	    in_order(n->r);
+	}
+    }
+
+    void
+    post_order() {
+	post_order(root);
+    }
+
+    void
+    post_order(node<T> *n) {
+	if (n->l != nullptr) {
+	    post_order(n->l);
+	}
+	if (n->r != nullptr) {
+	    post_order(n->r);
+	}
+	std::cout << n->val << "\n";
+    }
+
+    void
+    disp_pred(node<T> *n) {
+	std::cout << "pred of " << n->val << " = "
+		  << (this->find_predecessor(n) == nullptr
+			  ? -69
+			  : this->find_predecessor(n)->val)
+		  << "\n";
+    }
+    void
+    disp_succ(node<T> *n) {
+	std::cout << "succ of " << n->val << " = "
+		  << (this->find_successor(n) == nullptr
+			  ? -69
+			  : this->find_successor(n)->val)
+		  << "\n";
     }
 };
 
 int
 main() {
     bst<int> t;
-    node<int> *n = new node();
-    node<int> *m = new node();
-    node<int> *nn = new node();
-    m->val = 69;
+    node<int> *n = new node(-1);
+    node<int> *m = new node(1);
+    node<int> *nn = new node(10);
+    node<int> *mm = new node(-2);
+    node<int> *ll = new node(9);
+    node<int> *rr = new node(11);
     t.insert(n);
+    t.insert(m);
     t.insert(nn);
-    std::cout << t.find(n) << "\n";
-    std::cout << t.find(nn) << "\n";
-    std::cout << t.find(m) << "\n";
-    t.dfs();
+    t.insert(mm);
+    t.insert(ll);
+    t.insert(rr);
+    t.pre_order();
+    t.remove(nn);
+    t.pre_order();
 
     return 0;
 }
