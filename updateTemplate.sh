e566f40c22b7b74fa6038fa1a82d78e433eba754
Garzzza7 <piotr.czapela@gmail.com>
2025-12-04 10:42:09 +0100

    cleanup

--- updateTemplate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-template_file="TEMPLATE.cpp"
-
-destination_files=("CSES/TEMPLATE.cpp" "CodeForces/TODO/TEMPLATE.cpp" "CodeForces/TODO/contest/TEMPLATE.cpp" "Algos/TEMPLATE.cpp" "AtCoder/TEMPLATE.cpp" "SPOJ/TEMPLATE.cpp" "SZKOPUL/TEMPLATE.cpp" "ProjectEuler/TEMPLATE.cpp" "CodeForces/TODO/Miscellaneous/TEMPLATE.cpp" "AMPPZ/TEMPLATE.cpp")
-
-for dest_file in "${destination_files[@]}"; do
-	cp "$template_file" "$dest_file"
-done
