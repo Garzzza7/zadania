71acd091905eb065731607c235e6e1043879c856
Garzzza7 <piotr.czapela@gmail.com>
2026-05-02 21:47:19 +0200

    fix

--- SPOJ/Bitmap.cpp
+++ SPOJ/Bitmap.cpp
@@ -1,102 +1,3 @@
-/*
-#include <cmath>
-#include <cstdio>
-#include <iostream>
-#include <queue>
-#include <vector>
-
-#define INF 10000
-
-using namespace std;
-
-int a[190][190];
-int m, n;
-
-void
-BFS(int i, int j) {
-    queue<int> q;
-    q.push(i);
-    q.push(j);
-
-    // mark the current node as 0
-    a[i][j] = 0;
-
-    while (!q.empty()) {
-        // get the first two co-ordinates (x,y)
-        int x = q.front();
-        q.pop();
-        int y = q.front();
-        q.pop();
-
-        // check if it is possible to reach a neighbour in minimum steps
-
-        int dist = a[x][y] + 1;
-
-        // left
-        if (y - 1 >= 0 && dist < a[x][y - 1]) {
-            q.push(x);
-            q.push(y - 1);
-            a[x][y - 1] = dist;
-        }
-        // right
-        if (y + 1 < n && dist < a[x][y + 1]) {
-            q.push(x);
-            q.push(y + 1);
-            a[x][y + 1] = dist;
-        }
-
-        // up
-        if (x - 1 >= 0 && dist < a[x - 1][y]) {
-            q.push(x - 1);
-            q.push(y);
-            a[x - 1][y] = dist;
-        }
-
-        // down
-        if (x + 1 < m && dist < a[x + 1][y]) {
-            q.push(x + 1);
-            q.push(y);
-            a[x + 1][y] = dist;
-        }
-    }
-}
-
-int
-main(void) {
-    int t;
-    cin >> t;
-
-    while (t--) {
-        vector<pair<int, int>> vc;
-        cin >> m >> n;
-
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++) {
-                char ch;
-                cin >> ch;
-                // if the current character is 1...
-                if (ch == '1') {
-                    // mark the current distance as negative INFINITY
-                    a[i][j] = -INF;
-                    // add all the (i,j) into the vector.
-                    vc.emplace_back(i, j);
-                } else // current cell contains 0, hence mark this as INFINITY
-                    a[i][j] = INF;
-            }
-
-        // call BFS for each i,j in the vector vc
-        for (auto & i : vc)
-            BFS(i.first, i.second);
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++)
-                cout << a[i][j] << " ";
-            cout << endl;
-        }
-        cout << endl;
-    }
-}
-*/
 #pragma GCC optimize("Ofast")
 #include <algorithm>
 #include <cstdint>
@@ -213,8 +114,8 @@ solve2(void) {
                     continue;
                 }
                 if (not vis[c.first][c.second]) {
-                    if(vec[c.first][c.second] == '0') {
-                        res[c.first][c.second] = std::min(res[c.first][c.second] , res[curr.first][curr.second] + 1);
+                    if (vec[c.first][c.second] == '0') {
+                        res[c.first][c.second] = std::min(res[c.first][c.second], res[curr.first][curr.second] + 1);
                         q.push(c);
                     }
                 }
