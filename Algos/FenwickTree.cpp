f87d212c49b2cf9da4023054d2eee92ef6693e47
Garzzza7 <piotr.czapela@gmail.com>
2026-06-28 23:09:07 +0200

    fix fenwick

--- Algos/FenwickTree.cpp
+++ Algos/FenwickTree.cpp
@@ -1,56 +1,55 @@
 #include <iostream>
 #include <vector>
 
-// https://atcoder.jp/contests/practice2/tasks/practice2_b
+// tested on https://judge.yosupo.jp/problem/point_add_range_sum
 
 template <typename T, typename OP> struct fenwick {
-    /*
-     * ONE INDEXED !!!
-     * REMEMBER THIS !!!
-     */
+    // 0-indexed for queries and updates
   private:
-    int _size{0};
+    int _size;
     std::vector<T> _vec;
     static constexpr OP op{};
 
   public:
-    fenwick(const int &_n)
-        : _size(_n + 1) {
+    fenwick(const int &n)
+        : _size(n + 1) {
         _vec.assign(_size, 0);
     }
 
     fenwick(const std::vector<T> &init)
         : _size((int) init.size() + 1) {
         _vec.assign(_size, 0);
-        for (int i = 1; i < _size; i++) {
-            update(_vec[i - 1], i);
+        for (int i = 0; i < (int) init.size(); i++) {
+            add(i, init[i]);
         }
     }
 
     void
-    update(const T &val, int idx) {
+    add(int idx, const T &val) {
+        idx++;
         while (idx < _size) {
             _vec[idx] = op(_vec[idx], val);
-            idx += ((idx) & (-idx));
+            idx += idx & -idx;
         }
     }
 
-    T
-    query(const int &l, const int &r) {
-        auto calc = [&](int idx) -> T {
+    [[nodiscard]] T
+    query(const int &l, const int &r) const {
+        // <l , r>
+        auto calc = [this](int idx) -> T {
             idx++;
             T res = 0;
             while (idx > 0) {
                 res = op(res, _vec[idx]);
-                idx -= ((idx) & (-idx));
+                idx -= idx & -idx;
             }
             return res;
         };
-        return calc(r) - calc(l - 1);
+        return calc(r) - (l ? calc(l - 1) : 0);
     }
 };
 
-constexpr auto op = [](const auto &l, const auto &r) -> auto { return l + r; };
+constexpr auto op = [](const auto &a, const auto &b) { return a + b; };
 
 using fen         = fenwick<long long, decltype(op)>;
 
@@ -60,29 +59,29 @@ main(void) {
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    int n, m;
-    std::cin >> n >> m;
+    int n, q;
+    std::cin >> n >> q;
 
     fen fen(n);
 
-    for (int i = 1; i <= n; i++) {
-        long long a;
-        std::cin >> a;
-        fen.update(a, i);
+    for (int i = 0; i < n; i++) {
+        long long v;
+        std::cin >> v;
+        fen.add(i, v);
     }
 
-    while (m--) {
+    while (q--) {
         int a;
         std::cin >> a;
         if (a == 1) {
-            int b, c;
-            std::cin >> b >> c;
-            std::cout << fen.query(b, c - 1) << "\n";
+            int l, r;
+            std::cin >> l >> r;
+            std::cout << fen.query(l, r - 1) << "\n";
         } else {
-            int b;
-            long long c;
-            std::cin >> b >> c;
-            fen.update(c, b + 1);
+            int i;
+            long long v;
+            std::cin >> i >> v;
+            fen.add(i, v);
         }
     }
 
